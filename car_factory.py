0fce47e40a8d8b0dbc03d99a4b71525b17b0ca27
Snorman <ahs@Snormans-MacBook-Pro.local>
2024-08-07 14:30:24 +0800

    added unit tests to the codebase

---
+++ car_factory.py
@@ -0,0 +1,37 @@
+from car import Car
+from engine.capulet_engine import CapuletEngine
+from engine.willoughby_engine import WilloughbyEngine
+from engine.sternman_engine import SternmanEngine
+from battery.spindler_battery import SpindlerBattery
+from battery.nubbin_battery import NubbinBattery
+
+class CarFactory:
+    @staticmethod
+    def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = CapuletEngine(current_mileage, last_service_mileage)
+        battery = SpindlerBattery(current_date, last_service_date)
+        return Car(engine, battery)
+    
+    @staticmethod
+    def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = WilloughbyEngine(current_mileage, last_service_mileage)
+        battery = SpindlerBattery(current_date, last_service_date)
+        return Car(engine, battery)
+    
+    @staticmethod
+    def create_palindrome(current_mileage, last_service_mileage, warning_light_is_on):
+        engine = SternmanEngine(warning_light_is_on)
+        battery = SpindlerBattery(current_mileage, last_service_mileage)
+        return Car(engine, battery)
+    
+    @staticmethod
+    def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = WilloughbyEngine(current_mileage, last_service_mileage)
+        battery = NubbinBattery(current_date, last_service_date)
+        return Car(engine, battery)
+    
+    @staticmethod
+    def create_Thovex(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = WilloughbyEngine(current_mileage, last_service_mileage)
+        battery = NubbinBattery(current_date, last_service_date)
+        return Car(engine, battery)
